90ecd52c81e95d8715309142e4b280208278f394
gabims029 <gabimelo541@gmail.com>
2024-08-28 13:59:41 -0300

    Rota POST

--- primeiraAPI/src/index.js
+++ primeiraAPI/src/index.js
@@ -17,8 +17,41 @@ class AppController {
   }
   //Define as rotas da nossa API
   routes() {
+    const users =[];
+
+    this.express.post("/users",(req,res)=>{
+      const {id,nome,email,senha} = req.body
+      users.push({id,nome,email,senha});
+      res.status(200).send({message:"Usuário cadastrado com sucesso"});
+    });
+   
+    this.express.post("/auth",(req,res)=>{
+      const {email,senha} = req.body
+      const user = users.find(user => user.email == email && user.senha == senha);
+      if(user){
+        res.status(200).send({message: "Usuário encontrado"});
+      }
+      else{
+        res.status(400).send({message: "Usuário NÃO encontrado"});
+      }
+    });
+
+    this.express.get("/users/:id",(req,res)=>{
+      const {id} = req.params;
+      const user = users.find(user => user.id == id);
+      if(user){
+        res.status(200).send(user);
+      }
+      else{
+        res.status(400).send({message: "Usuário não encontrado"});
+      }
+    });
+
+
+
+
     //Define uma rota GET para o caminho health
-    this.express.get("/health/", (req, res) => {
+    this.express.get("/health/", (req, res)=>{
       res.send({ status: "OK" });
     });//Essa rota é usada para verificar se a API estoa OK
   }
